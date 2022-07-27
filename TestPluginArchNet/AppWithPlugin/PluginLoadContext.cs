7678099a436dd6c726adb22ba3697c292e6c9259
PKkDev <prodevkir@mail.ru>
2022-07-27 11:02:51 +0400

    first commit

---
+++ TestPluginArchNet/AppWithPlugin/PluginLoadContext.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Runtime.Loader;
+
+namespace AppWithPlugin
+{
+    /*
+    Тип AssemblyLoadContext — это специальный тип в среде выполнения, 
+    который позволяет разработчикам изолировать загруженные сборки в разные группы, 
+    чтобы версии сборок не конфликтовали друг с другом
+     */
+
+    class PluginLoadContext : AssemblyLoadContext
+    {
+        private readonly AssemblyDependencyResolver _resolver;
+
+        public PluginLoadContext(string pluginPath)
+        {
+            _resolver = new AssemblyDependencyResolver(pluginPath);
+        }
+
+        protected override Assembly? Load(AssemblyName assemblyName)
+        {
+            string? assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+            if (assemblyPath != null)
+            {
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+
+            return null;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            string? libraryPath = _resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+            if (libraryPath != null)
+            {
+                return LoadUnmanagedDllFromPath(libraryPath);
+            }
+
+            return IntPtr.Zero;
+        }
+    }
+}
